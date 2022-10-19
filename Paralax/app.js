5ab256c8e074995ecdff329072dca2ab55a97ace
TOKYO <nezexmain@gmail.com>
2022-10-19 02:35:01 +0200

    'paralax'

---
+++ Paralax/app.js
@@ -0,0 +1,9 @@
+const sliderMain = new Swiper('.slider_main',{
+    freeMode: true,
+    parallax: true,
+    slidesPerView:3.5,
+    centeredSlides: true,//false
+    spaceBetween: 60,
+    mousewheel:true,
+    
+})
